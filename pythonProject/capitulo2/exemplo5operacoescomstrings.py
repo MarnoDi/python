dedaa4c10de5f32a7b531962ee25ecb23b6d4c6f
Julia <julia.boma.silva@escola.pr.gov.br>
2025-05-09 11:18:49 -0300

    exemplo 5 operacoes

---
+++ pythonProject/capitulo2/exemplo5operacoescomstrings.py
@@ -0,0 +1,10 @@
+# é proibido utilizar operações matematicas com strings
+# ao menos em 2 casos
+
+first = 'jose'
+second = ' valim'
+nomeSobrenome = first + second # o mais aq significa concatenação
+print(nomeSobrenome)
+
+stringQualquer = "teste " * 3
+print(stringQualquer)
