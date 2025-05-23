d515b473b1c2666bbb1d2572d0c53a95e3f0a8c7
Julia <julia.boma.silva@escola.pr.gov.br>
2025-05-23 10:55:37 -0300

    exercicio 2 lista

---
+++ pythonProject/capitulo2/lista/exercicio1-lista.py
@@ -0,0 +1,12 @@
+numero1 = int(input('escreva o primeiro numero: '))
+numero2 = int(input('escreva o segundo numero: '))
+
+soma = numero1 + numero2
+produto = numero1 * numero2
+diferenca = numero1 - numero2
+quociente = numero1 / numero2
+
+print('a soma deu:', soma)
+print('o produto deu:', produto)
+print('a diferenca deu:', diferença)
+print('o quociente deu:', quociente)
---
+++ pythonProject/capitulo2/lista/exercicio2-lista.py
@@ -0,0 +1,15 @@
+numero = int(input("Digite um número de 5 dígitos: "))
+
+numero1 = (numero // 10000) % 10
+numero2 = (numero // 1000) % 10
+numero3 = (numero // 100) % 10
+numero4 = (numero // 10) % 10
+numero5 = numero % 10
+
+#
+
+print("  ", numero1)
+print("  ", numero2)
+print("     ", numero3)
+print("  ", numero4)
+print("  ", numero5)
