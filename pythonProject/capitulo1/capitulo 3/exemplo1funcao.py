92867393d718de7b44218999035b9872b5dd9dbb
Lucas <ana.santos.ribeiro01@escola.pr.gov.br>
2025-05-30 09:58:55 -0300

    exemplo1funcao

---
+++ pythonProject/capitulo1/capitulo 3/exemplo1funcao.py
@@ -0,0 +1,13 @@
+# função : sequencia noemada de instruçoes que executa uma operação de computação
+# uma função precisa ser chamada.
+# argumento: algo passado pela função
+# uma funcão tem um nome seguido por parenteses ()
+#para definir uma funçao para variavel é pelo =, o = é uma variavel e  o () é uma funcão
+#
+# ----------------------------------------------------------------------------------------
+# A função int converte um numero em inteiro
+string = '32'
+print(type(string))
+print(type(int('32')))
+int('22')
+# int('hello') erro por que a funcao int() nao testa caracteres que nao seja numericas
