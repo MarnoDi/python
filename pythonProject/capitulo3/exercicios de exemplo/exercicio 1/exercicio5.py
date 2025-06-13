937285a6afb3432e87edc41c764329d9751e5442
pablo <pablo.fesqueiras@escola.pr.gov.br>
2025-06-13 12:20:30 -0300

    exercicio 5

---
+++ pythonProject/capitulo3/exercicios de exemplo/exercicio 1/exercicio5.py
@@ -0,0 +1,12 @@
+def calculoMedia(calculoMedia):
+    return (n1 + n2 + n3 + n4) / 4
+
+#main
+n1 = float(input("digite a nota: "))
+n2 = float(input("digite a nota: "))
+n3 = float(input("digite a nota: "))
+n4 = float(input("digite a nota: "))
+
+media = calculoMedia(calculoMedia)
+
+print("a media é de : ", media)
