009d57710ff5d6e155fa65c67ecbf4617a70e190
pablo <pablo.fesqueiras@escola.pr.gov.br>
2025-06-13 10:56:57 -0300

    exemplo exercicio 1

---
+++ pythonProject/capitulo3/exercicios de exemplo/exercicio 1/exercicio1.py
@@ -0,0 +1,12 @@
+def soma(numero, numero2):
+    print(numero + numero2)
+
+def subtracao(numero, numero2):
+    print(numero - numero2)
+
+
+numero = int(input("digite o 1° numero:")) # input recebe entrada do usuario
+numero2 = int(input("digite o 2° numero:")) # int converte aquilo em inteiros
+
+soma(numero, numero2)
+subtracao(numero, numero2)
--- pythonProject/capitulo3/lista/exercicio1.py
+++ pythonProject/capitulo3/lista/exercicio1.py
@@ -1,7 +1,21 @@
-def justificar_direita(stringQualquer):
-    setentaEspaços = "           "
-    print(setentaEspaços*7 + stringQualquer)
+def soma(numero, numero2):
+    print("o resultado da soma é:", numero + numero2)
+
+def subtracao(numero, numero2):
+    print("o resultado da subtração é:", numero - numero2)
+
+def multiplicacao(numero, numero2):
+    print("o resultado da multiplicacao é:", numero * numero2)
+
+def divisao(numero, numero2):
+    resultado_divisao = numero / numero2
+    print("o resultado da divisao é: ", resultado_divisao)
 
 
-stringQualquer  "Endrius"
-justificar_direita(stringQualquer)
+numero = int(input("digite o 1° numero:")) # input recebe entrada do usuario
+numero2 = int(input("digite o 2° numero:")) # int converte aquilo em inteiros
+
+soma(numero, numero2)
+subtracao(numero, numero2)
+multiplicacao(numero, numero2)
+divisao(numero, numero2)
