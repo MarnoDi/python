ca5052d0fc68656c81348581fef6bd6552aa5966
pablo <pablo.fesqueiras@escola.pr.gov.br>
2025-06-13 12:03:42 -0300

    exercicio 3 e 4

---
+++ pythonProject/capitulo3/exercicios de exemplo/exercicio 1/exercicio3.py
@@ -0,0 +1,17 @@
+def diametro(diametro):
+    print("o diametro deu", 2 * raio)
+
+def circunferencia(circunferencia):
+    print("a circunferencia deu: ", 2 * pi * raio)
+
+def area(area):
+    print("a area deu:", pi * (raio ** 2))
+
+#main
+
+raio = int(input("digite o numero do raio: "))
+pi = 3.14159
+
+diametro(diametro)
+circunferencia(circunferencia)
+area(area)
