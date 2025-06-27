5d853162a393c749f07d4e8581f9c8ea1e5504fd
mateus <mateus.gomes.dias@escola.pr.gov.br>
2025-06-27 12:17:16 -0300

    exercicio python

---
+++ pythonProject/capitulo3/exercicios de exemplo/exercicio 1/exercicio8.py
@@ -0,0 +1,14 @@
+def do_twice(desenho):
+   desenho()
+def desenho():
+   print('+', '- - - - ', '+', '- - - - ', '+')
+   print('|          |          |')
+   print('|          |          |')
+   print('|          |          |')
+   print('|          |          |')
+
+do_twice(desenho)
+do_twice(desenho)
+def desenho2():
+   print('+', '- - - - ', '+', '- - - - ', '+')
+do_twice(desenho2)
